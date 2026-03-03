a435033b5a210fbd0a605a974b087fc0ddd67842
lucassilva18byte <lucas.santossilva1906@gmail.com>
2026-03-03 19:53:41 +0000

    teste14

--- script.js
+++ script.js
@@ -1,8 +1,49 @@
-const scene = document.querySelector(".scene");
+const image = document.querySelector(".main-image");
+const canvas = document.getElementById("particles");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particles = [];
+
+for (let i = 0; i < 40; i++) {
+  particles.push({
+    x: Math.random() * canvas.width,
+    y: canvas.height + Math.random() * 200,
+    size: Math.random() * 3 + 1,
+    speed: Math.random() * 1 + 0.5
+  });
+}
+
+function animateParticles() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = "rgba(255,120,0,0.7)";
+
+  particles.forEach(p => {
+    ctx.beginPath();
+    ctx.arc(p.x, p.y, p.size, 0, Math.PI * 2);
+    ctx.fill();
+
+    p.y -= p.speed;
+
+    if (p.y < 0) {
+      p.y = canvas.height + 50;
+      p.x = Math.random() * canvas.width;
+    }
+  });
+
+  requestAnimationFrame(animateParticles);
+}
+
+animateParticles();
+
+/* PARALLAX SUAVE */
 
 document.addEventListener("mousemove", (e) => {
-  const x = (e.clientX / window.innerWidth - 0.5) * 10;
-  const y = (e.clientY / window.innerHeight - 0.5) * 10;
+  const x = (e.clientX / window.innerWidth - 0.5) * 20;
+  const y = (e.clientY / window.innerHeight - 0.5) * 20;
 
-  scene.style.transform = `translate(${x}px, ${y}px)`;
+  image.style.transform = `scale(1.04) translate(${x}px, ${y}px)`;
 });
