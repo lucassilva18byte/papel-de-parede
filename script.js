dd7598660dce7967186740f333d320388a46e196
lucassilva18byte <lucas.santossilva1906@gmail.com>
2026-03-03 19:23:52 +0000

    teste9

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const scene = document.querySelector(".scene");
 
 document.addEventListener("mousemove", (e) => {
-  let x = (e.clientX / window.innerWidth - 0.5) * 20;
-  let y = (e.clientY / window.innerHeight - 0.5) * 20;
+  let x = (e.clientX / window.innerWidth - 0.5) * 10;
+  let y = (e.clientY / window.innerHeight - 0.5) * 10;
 
   scene.style.transform = `translate(${x}px, ${y}px)`;
 });
